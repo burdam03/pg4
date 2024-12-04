0e29729f5095a538add7b080151459a58efe2527
burdam03 <burdam03@jcu.cz>
2024-12-04 19:40:48 +0000

    úkol8

---
+++ eighth.py
@@ -0,0 +1,20 @@
+def bin_to_dec(binarni_cislo):
+    cislo = str(binarni_cislo)
+    soucet = 0
+
+    for index, cislo in enumerate(num):
+        exponent = len(num) - index - 1
+        if cislo == "1":
+            print(exponent)
+            soucet += 2 ** exponent 
+
+
+    return soucet
+
+def test_funkce():
+    assert bin_to_dec("0") == 0
+    assert bin_to_dec(1) == 1
+    assert bin_to_dec("100") == 4
+    assert bin_to_dec(101) == 5
+    assert bin_to_dec("010101") == 21
+    assert bin_to_dec(10000000) == 128
